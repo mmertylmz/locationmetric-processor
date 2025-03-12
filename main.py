dfa80752299177f9a0a679cb58a47b4d42db035c
Mert Can Yılmaz <dokkensavatage@gmail.com>
2025-03-12 12:16:12 +0300

    test

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("demo")
+import pandas as pd
+
+def read_excel_columns(file_path):
+    try:
+        df = pd.read_excel(file_path)
+
+        print("\nFirst Five Rows")
+        print(df.head())
+
+        return df
+
+    except FileNotFoundError:
+        print(f"Error: '{file_path}' file could not found.")
+    except Exception as e:
+        print(f"An error occurred: '{e}'")
+    return ""
+
+
+if __name__ == "__main__":
+    file_path = "Excel Files/Outscraper.xlsx"
+    df = read_excel_columns(file_path)
